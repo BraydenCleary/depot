35766b1e3a274c7bdff5081399241f31bd3bbb8a
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-02 19:02:14 -0500

    wrote tests for all product model validations

--- test/unit/product_test.rb
+++ test/unit/product_test.rb
@@ -1,13 +1,64 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  
+  fixtures :products
+
   test 'product attributes must not be empty' do
 		product = Product.new
 		assert product.invalid?
 		assert product.errors[:title].any?
 		assert product.errors[:description].any?
 		assert product.errors[:price].any?
-		assert product.errors[:price].any?  	
+		assert product.errors[:price].any? 
   end
+
+  test 'product price must be positive' do
+  	product = Product.new(:title => 'Brayden is Awesome',
+  												:description => 'Whoa he is smart!',
+  												:image_url => 'brayden.jpg')
+  	product.price = -1
+  	assert product.invalid?
+  	assert_equal "must be greater than or equal to 0.01",
+  		product.errors[:price].join('; ')
+
+  	product.price = 0
+  	assert product.invalid?
+		assert_equal "must be greater than or equal to 0.01",
+  		product.errors[:price].join('; ')
+
+  	product.price = 1
+  	assert product.valid?  	
+  end
+
+  test 'image ends with correct extension' do
+
+  	def new_product(image_url)
+  		Product.new(:title => 'hello',
+  								:description => "this is the world's best book",
+  								:image_url => image_url,
+  								:price => 9.99)
+  	end
+
+  	good = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg http://a.b.c/x/y/z/fred.gif }
+
+  	bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+  	good.each do |name|
+  		assert new_product(name).valid?, "#{name} shouldn't be invalid"
+  	end
+
+  	bad.each do |name|
+  		assert new_product(name).invalid?, "#{name} shouldn't be valid"
+  	end
+  end
+
+  test 'product is not valid without  a unqiue title' do
+  	product = Product.new(:title => products(:ruby).title,
+  												:description => 'yyy',
+  												:price => 1,
+  												:image_url => 'fred.gif')
+  	assert !product.save
+  	assert_equal 'has already been taken', product.errors[:title].join('; ')
+  end
+
 end
