1d02df08491573aad19d3ece94e5afe6ab2e5969
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-02 22:58:43 -0500

    created basic shopping cart

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,7 +3,7 @@ class Product < ActiveRecord::Base
 
   has_many :line_items
 
-  before_destory :ensure_not_referenced_by_any_line_item
+  before_destroy :ensure_not_referenced_by_any_line_item
 
   attr_accessible :description, :image_url, :price, :title
 
@@ -19,12 +19,12 @@ class Product < ActiveRecord::Base
 	private
 
 		def ensure_not_referenced_by_any_line_item
-			if line_tems.empty?
+			if line_items.empty?
 				return true
 			else
 				errors.add(:base, 'Line Items present')
 				return false
 			end
 		end
-	
+
 end
