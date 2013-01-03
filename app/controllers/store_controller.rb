b4313a78f086f835c3f98d36648e4b4f914fc7c0
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-03 10:55:24 -0500

    Finished Chapter 9 Challenges

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+
   def index
   	@products = Product.all
+
+  	@session_counter = session[:counter]
+  	if @session_counter.nil?
+  		@session_counter = 1
+  	else
+  		@session_counter += 1
+  	end
+
   end
 end
