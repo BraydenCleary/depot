ab4de27275e1f332122304180a04ced5e6591931
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-03 12:20:05 -0500

    implemented cart total

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -1,5 +1,9 @@
 class LineItem < ActiveRecord::Base
-	attr_accessible :product_id, :cart_id, :product
+	attr_accessible :product_id, :cart_id, :product, :quantity
   belongs_to :product
   belongs_to :cart
+
+  def total_price
+  	product.price * quantity
+ 	end
 end
