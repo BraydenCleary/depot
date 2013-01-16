5b2ce2d8fb167a81f4df68a86b6a9534f5e68400
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-16 11:25:30 -0500

    ensured cannot delete last user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ActiveRecord::Base
 	attr_accessible :name, :password, :password_confirmation
 
+	after_destroy :ensure_an_admin_remains
+
+	def ensure_an_admin_remains
+		if User.count.zero?
+			raise "Can't delete last user"
+		end
+	end
+
   validates :name, :presence => true, :uniqueness => true
 
   validates :password, :confirmation => true
