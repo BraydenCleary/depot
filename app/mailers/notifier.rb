cfcb2655957049124f3da0aa509335095bba87d7
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-16 10:09:55 -0500

    finishing mailers

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -9,7 +9,7 @@ class Notifier < ActionMailer::Base
   def order_received(order)
     @order = order
     
-    mail :to => order.email, :subject => "Pragmatic Store Order Confirmation"
+    mail :to => order.email, :subject => "Pragmatic Store Order Confirmation "
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -21,6 +21,6 @@ class Notifier < ActionMailer::Base
 
     @greeting = "Hi"
 
-    mail to: "to@example.org"
+    mail to: => order.email, :subject => "Pragmatic Store Order Shipped"
   end
 end
