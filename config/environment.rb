383aa7e64adb78174f40a6ae6ceb465a49ae88e9
Brayden Cleary <brayden.cleary@gmail.com>
2013-01-16 09:50:29 -0500

    building order mailers

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,18 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Depot::Application.initialize!
+
+Depot::Application.configure do
+	config.action_mailer.deliver_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+		:address => "smtp.gmail.com",
+		:port => 587,
+		:domain => "brayden@example.com",
+		:authenticaiton => "plain",
+		:user_name => "brayden",
+		:password => "secret",
+		:enable_starttls_auto => true
+	}
+end
+
